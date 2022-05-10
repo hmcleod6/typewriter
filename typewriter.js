2beb55dbafb0534f34732aa47d3da84c3756717c
Heather <hmcleod29@gmail.com>
2022-05-10 13:37:33 -0400

    Adding the code to write the animated sentence in

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "my name is heather mcleod\n";
+
+let time = 1000;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time) 
+  time += 50;// <= 1s delay to make it noticeable. Can dial it down later.
+};

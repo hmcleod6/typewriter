56276c1a49a535134788d431417f7aefc453a1cc
Heather <hmcleod29@gmail.com>
2022-05-10 13:38:09 -0400

    Linted and submitted

--- typewriter.js
+++ typewriter.js
@@ -5,6 +5,6 @@ let time = 1000;
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
-  }, time) 
+  }, time);
   time += 50;// <= 1s delay to make it noticeable. Can dial it down later.
-};
+}
